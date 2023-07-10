19f2f569d768fe23d00b6aad49a1d3a719e04260
Shrestha <138806766+shresthacodes@users.noreply.github.com>
2023-07-10 18:31:06 +0000

    spy number

--- TYL Java/program1.java
+++ TYL Java/program1.java
@@ -1,13 +1,13 @@
+
 public class program1 {
-    public static void public static void main(String[] args) {
-       int i,j,row = 6;
-       for (i=0;i<n;i++)
-       {
-        for (j=0;j<i;j++){
-            System.out.println("*");
+    public static void main(String[] args) {
+        int i, j, row = 6;
+        for (i = 0; i < row; i++) {
+            for (j = 0; j < i; j++) {
+                System.out.println("*");
+            }
+            System.out.println();
         }
-        System.out.println();
-       }
 
     }
 }
