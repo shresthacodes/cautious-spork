2446b8da45346eedc2cf20249b9d4b71463674b9
shresthacodes <invinciblecodes@gmail.com>
2023-07-31 22:35:40 +0530

    avg of two test marks

--- python lab/program1.py
+++ python lab/program1.py
@@ -1,1 +1,9 @@
-
+# WAP to find the best of two average test marks out of 3 test marks accepted from the user
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
+tl = a+b+c
+min = min(a, b, c)
+b_a = tl-min
+avg = b_a/2
+print("Best of two average test marks out of 3 test marks is: ", avg)
